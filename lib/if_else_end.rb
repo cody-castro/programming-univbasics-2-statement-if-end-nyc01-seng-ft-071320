b48d6a6d214157269122735142624ba906070712
Cody C <codyicastro@gmail.com>
2020-06-26 13:39:59 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -2,6 +2,10 @@
 current_time = Time.now
 
 #if time.second is even
-if current_time.to_i == current_time.to_i 
+if current_time.to_i == current_time.to_i.even?
 # puts "even!"
+  puts "even!"
 # else puts "odd!"
+else 
+  puts "odd"
+end
